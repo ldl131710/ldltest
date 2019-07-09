67bb081f608deec24eb36290c576758d17e54e2d
ldl <ldl131710@163.comm>
2019-07-09 13:09:26 +0800

    项目搭建

---
+++ src/main/java/aaa/ldltest/LdltestApplication.java
@@ -0,0 +1,13 @@
+package aaa.ldltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdltestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdltestApplication.class, args);
+    }
+
+}
