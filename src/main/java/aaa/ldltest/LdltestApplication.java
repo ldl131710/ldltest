78b5d2270aab8db9760e1b5d8d104c458be47a66
ldl <ldl131710@163.comm>
2019-07-09 13:22:19 +0800

    客户端修改测试

--- src/main/java/aaa/ldltest/LdltestApplication.java
+++ src/main/java/aaa/ldltest/LdltestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LdltestApplication {
 
     /**
-     * 测试
+     * 测试123
      * @param args
      */
     public static void main(String[] args) {
